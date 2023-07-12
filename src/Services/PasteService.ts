c2d0b24719a3102ef89fafdb4c1f4aa2912a67f7
Priyanshu Bharti <priyanshub25@gmail.com>
2023-07-12 20:10:25 +0530

    Created User Services

--- src/Services/PasteService.ts
+++ src/Services/PasteService.ts
@@ -11,10 +11,12 @@ export const getAllPasteFromDb = async () => {
     const allPastes = await PasteDbModel.find();
     return allPastes;
 };
+
 export const getPasteByIdFromDb = async (pasteId: string) => {
     const paste = await PasteDbModel.findById(pasteId);
     return paste;
 };
+
 export const deletePasteByIdFromDb = async (pasteId: string) => {
     const result = await PasteDbModel.findByIdAndRemove(pasteId);
     return result;
--- src/Services/UserService.ts
+++ src/Services/UserService.ts
@@ -7,7 +7,20 @@ export const saveUserToDb = async (user: UserModel) => {
     return true;
 };
 
-export const findUserByIdFromDb = async (pasteId: string) => {
-    const user = await UserDbModel.findById(pasteId);
+export const findUserByIdFromDb = async (id: string) => {
+    const user = await UserDbModel.findById(id);
     return user;
+};
+
+export const deleteUserByIdFromDb = async (id: string) => {
+    const result = await UserDbModel.findByIdAndRemove(id);
+    return result;
+};
+
+export const updateUserToDb = async (id: string, paste: UserModel) => {
+    const updatedPaste = await UserDbModel.updateOne(
+        { _id: id },
+        { _id: id, ...paste }
+    );
+    return updatedPaste;
 };
